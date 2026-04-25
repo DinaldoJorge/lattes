0986eedc1a18a6e3d40bb9c40e25eef5333225f3
DinaldoJorge <45379006+DinaldoJorge@users.noreply.github.com>
2026-04-25 16:24:51 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,13 +1,17 @@
 import streamlit as st
 
-col1, col2, col3 = st.columns([1,2,1])
-
-# CENTRO (logo)
-with col2:
+# TOPO (imagem centralizada)
+col_top1, col_top2, col_top3 = st.columns([1,2,1])
+with col_top2:
     st.image("star.png")
 
-# ESQUERDA (nome + imagem + botão)
+# ABAIXO DA IMAGEM (conteúdo lado a lado)
+col1, col2 = st.columns([1,2])
+
 with col1:
     st.write("Nome Dinaldo Jorge Guedes Santos")
     st.image("teste.png")
     st.link_button("Acessar", "http://lattes.cnpq.br/4494611683890258")
+
+with col2:
+    st.empty()  # pode colocar algo aqui depois
