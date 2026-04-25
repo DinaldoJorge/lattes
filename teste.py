d8916a4e8e3b5df255bb2964bf7fed322644c6d3
DinaldoJorge <45379006+DinaldoJorge@users.noreply.github.com>
2026-04-25 16:31:27 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,9 +1,25 @@
 import streamlit as st
 
-# TOPO (imagem centralizada)
+# CONFIGURAÇÃO DA PÁGINA
+st.set_page_config(
+    page_title="Perfil",
+    layout="wide"
+)
+
+# TOPO (imagem centralizada e clicável)
 col_top1, col_top2, col_top3 = st.columns([1,2,1])
+
 with col_top2:
-    st.image("star.png")
+    st.markdown(
+        """
+        <div style="text-align: center;">
+            <a href="https://starlink.com/" target="_blank">
+                <img src="star.png" width="300">
+            </a>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
 # ABAIXO DA IMAGEM (conteúdo lado a lado)
 col1, col2 = st.columns([1,2])
@@ -14,4 +30,4 @@ with col1:
     st.link_button("Acessar", "http://lattes.cnpq.br/4494611683890258")
 
 with col2:
-    st.empty()  # pode colocar algo aqui depois
+    st.empty()  # espaço para conteúdo futuro
