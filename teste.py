c0c9c316d37fa07197ace1cdbc9cfdea2e4f5cf8
DinaldoJorge <45379006+DinaldoJorge@users.noreply.github.com>
2026-04-25 16:35:02 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,27 +1,36 @@
 import streamlit as st
 
-# CONFIGURAÇÃO DA PÁGINA
-st.set_page_config(
-    page_title="Perfil",
-    layout="wide"
-)
+# CONFIGURAÇÃO
+st.set_page_config(page_title="Perfil", layout="wide")
 
-# TOPO (imagem centralizada e clicável)
+# CSS para deixar a imagem clicável
+st.markdown("""
+<style>
+.link-img {
+    display: flex;
+    justify-content: center;
+}
+.link-img a {
+    text-decoration: none;
+}
+</style>
+""", unsafe_allow_html=True)
+
+# TOPO (imagem centralizada + link)
 col_top1, col_top2, col_top3 = st.columns([1,2,1])
 
 with col_top2:
-    st.markdown(
-        """
-        <div style="text-align: center;">
-            <a href="https://starlink.com/" target="_blank">
-                <img src="star.png" width="300">
-            </a>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+    st.markdown('<div class="link-img">', unsafe_allow_html=True)
+    
+    # imagem normal (garante que carrega)
+    st.image("star.png", width=300)
+    
+    # botão invisível abaixo (funciona como link)
+    st.link_button("🔗 Acessar Starlink", "https://starlink.com/")
+    
+    st.markdown('</div>', unsafe_allow_html=True)
 
-# ABAIXO DA IMAGEM (conteúdo lado a lado)
+# ABAIXO
 col1, col2 = st.columns([1,2])
 
 with col1:
@@ -30,4 +39,4 @@ with col1:
     st.link_button("Acessar", "http://lattes.cnpq.br/4494611683890258")
 
 with col2:
-    st.empty()  # espaço para conteúdo futuro
+    st.empty()
