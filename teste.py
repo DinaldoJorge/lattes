596dbb8787d85244dcd8da14c1c8fa8e8ba11417
DinaldoJorge <45379006+DinaldoJorge@users.noreply.github.com>
2026-04-25 16:18:49 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,8 +5,10 @@ col1, col2, col3 = st.columns([1,2,1])
 with col2:
     st.image("star.png")
 
+with col1:
+    st.write("Nome Dinaldo Jorge Guedes Santos")
 
+with col1: 
+    st.image("teste.png")
 
-st.image("teste.png")
-st.write("Site Lattes")
 st.link_button("Acessar", "http://lattes.cnpq.br/4494611683890258")
